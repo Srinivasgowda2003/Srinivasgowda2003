80c1776d992afaf545d6323c6ae81c64c9726019
Srinivas Gowda <srinivasgowdagg2003@gmail.com>
2025-09-29 22:44:54 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,3 +43,8 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+// Navbar scroll effect
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('scrolled', window.scrollY > 50);
+});
